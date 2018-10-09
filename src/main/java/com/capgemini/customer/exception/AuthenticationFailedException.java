8d597c7d77d97a4e60956d209e13c6d942b9b7ee
reshek <reshek@IN-MUM-LDVM008.corp.capgemini.com>
2018-10-09 10:18:20 -0400

    1commit

---
+++ src/main/java/com/capgemini/customer/exception/AuthenticationFailedException.java
@@ -0,0 +1,12 @@
+package com.capgemini.customer.exception;
+
+public class AuthenticationFailedException extends Exception {
+
+	public AuthenticationFailedException(String message) {
+		// TODO Auto-generated constructor stub
+		
+		
+		super(message);
+	}
+
+}
---
+++ src/main/java/com/capgemini/customer/exception/CustomerAlreadyRegisteredException.java
@@ -0,0 +1,12 @@
+package com.capgemini.customer.exception;
+
+public class CustomerAlreadyRegisteredException extends Exception {
+
+	public CustomerAlreadyRegisteredException(String message) {
+		// TODO Auto-generated constructor stub
+
+	super(message);
+	}
+	
+
+}
